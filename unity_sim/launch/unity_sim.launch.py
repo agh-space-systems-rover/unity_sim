4f02403523daaadd2f2d262f939f9100afc96ff2
rayferric <63957587+rayferric@users.noreply.github.com>
2024-02-22 20:21:52 +0100

    feat: ability to launch Gazebo along the stack (still won't work without simulated depth sensors) + other changes

--- unity_sim/launch/unity_sim.launch.py
+++ unity_sim/launch/unity_sim.launch.py
@@ -1,5 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python import get_package_share_path
 
 
 def generate_launch_description():
@@ -10,5 +13,15 @@ def generate_launch_description():
             Node(package="rosbridge_server", executable="rosbridge_websocket"),
             # custom Unix Socket server for camera image transfer
             Node(package="unity_rs_publisher", executable="unity_rs_publisher"),
+            # robot structure TF publisher
+            IncludeLaunchDescription(
+                PythonLaunchDescriptionSource(
+                    str(
+                        get_package_share_path("kalman_description")
+                        / "launch"
+                        / "robot_state_publisher.launch.py"
+                    )
+                ),
+            ),
         ]
     )
