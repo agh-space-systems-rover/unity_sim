ec579b4febdfcba3f3d2c07c3d710d3985c2c941
Kacper Iwicki <kacper.iwi@gmail.com>
2024-12-03 19:14:02 +0100

    working as articulation

---
+++ unity_project/unity_sim/Assets/Simulation/Kalman/Arm/JointController.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public enum JointType
+{
+    Normal,
+    Differential,
+    Gripper
+}
+
+public enum JointId
+{
+    Joint1,
+    Joint2,
+    Joint3,
+    Joint4,
+    Joint5,
+    Joint6,
+    Gripper
+}
+
+public enum ControlMode
+{
+    SPEED,
+    POSITION,
+    POS_VEL,
+}
+
+public class JointController : MonoBehaviour
+{
+    [SerializeField]
+    public JointId jointId;
+
+    private ArticulationBody articulation;
+
+    private ControlMode mode = ControlMode.POS_VEL;
+
+    private float targetPosition, diffPosition;
+    private float targetVelocity, diffVelocity;
+
+    private PID pid = new PID(1.0f, 0.0f, 0.0f);
+    private const float maxSpeed = 30.0f;
+
+    public static Dictionary<JointId, JointType> jointTypes = new Dictionary<JointId, JointType>
+    {
+        {JointId.Joint1, JointType.Normal},
+        { JointId.Joint2, JointType.Normal},
+        { JointId.Joint3, JointType.Normal},
+        { JointId.Joint4, JointType.Normal},
+        { JointId.Joint5, JointType.Differential},
+        { JointId.Joint6, JointType.Differential},
+        { JointId.Gripper, JointType.Gripper}
+    };
+
+    public static Dictionary<JointId, JointId> differentialJoints = new Dictionary<JointId, JointId>
+    {
+        {JointId.Joint5, JointId.Joint6},
+        {JointId.Joint6, JointId.Joint5}
+    };
+
+
+    private float[] gearRatio = { 0.0125f, 0.00625f, 0.00625f, 0.01f, 0.0131034f, 0.0131034f, 1.0f };
+    private float[] invertDirection = { -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f };
+
+    // LIFE CYCLE
+
+    void Start()
+    {
+        articulation = GetComponent<ArticulationBody>();
+        targetPosition = CurrentPrimaryAxisRotation();
+        targetVelocity = 0;
+    }
+
+    void FixedUpdate()
+    {
+        if (articulation == null)
+        {
+            articulation = GetComponent<ArticulationBody>();
+            if (articulation == null)
+            {
+                UnityEngine.Debug.LogWarning($"Joint {jointId} not initialized");
+                return;
+            }
+            targetPosition = CurrentPrimaryAxisRotation();
+        }
+
+
+        if (jointTypes[jointId] == JointType.Gripper)
+        {
+            RotateTo(targetPosition);
+        }
+        else if (jointTypes[jointId] == JointType.Differential)
+        {
+            float realVelocity = (diffVelocity - invertDirection[(int)jointId] * targetVelocity) / 2;
+            if (mode == ControlMode.POS_VEL)
+            {
+                if (Math.Abs(realVelocity) > 0.001)
+                    targetPosition = (float)(CurrentPrimaryAxisRotation() + realVelocity * invertDirection[(int)jointId]);
+            }
+            float positionChange = Mathf.Clamp(pid.Update(CurrentPrimaryAxisRotation(), targetPosition, Time.fixedDeltaTime), -maxSpeed * Time.fixedDeltaTime, maxSpeed * Time.fixedDeltaTime);
+            RotateTo(CurrentPrimaryAxisRotation() + positionChange);
+        }
+        else
+        {
+            if (mode == ControlMode.POS_VEL)
+            {
+                if (Math.Abs(targetVelocity) > 0.001)
+                    targetPosition = (float)(CurrentPrimaryAxisRotation() + targetVelocity * invertDirection[(int)jointId]);
+            }
+            float positionChange = Mathf.Clamp(pid.Update(CurrentPrimaryAxisRotation(), targetPosition, Time.fixedDeltaTime), -maxSpeed * Time.fixedDeltaTime, maxSpeed * Time.fixedDeltaTime);
+            // if (jointId == JointId.Joint1)
+            // {
+            //     UnityEngine.Debug.Log($"Joint {jointId} position {CurrentPrimaryAxisRotation()} target {targetPosition} change {positionChange}");
+            // }
+            // UnityEngine.Debug.Log($"Joint {jointId} position {CurrentPrimaryAxisRotation()} target {targetPosition} change {positionChange}");
+            RotateTo(CurrentPrimaryAxisRotation() + positionChange);
+        }
+    }
+
+
+    // MOVEMENT HELPERS
+
+    float CurrentPrimaryAxisRotation()
+    {
+        if (articulation == null)
+        {
+            return 0;
+        }
+        float currentRotationRads = articulation.jointPosition[0];
+        float currentRotation = Mathf.Rad2Deg * currentRotationRads;
+        return currentRotation;
+    }
+
+    void RotateTo(float primaryAxisRotation)
+    {
+        var drive = articulation.xDrive;
+        drive.target = primaryAxisRotation;
+        articulation.xDrive = drive;
+    }
+
+    // PUBLIC INTERFACE
+
+    public void SetControlMode(ControlMode mode)
+    {
+        this.mode = mode;
+    }
+
+    public void SetTargetPosition(float targetPosition)
+    {
+        if (jointId == JointId.Joint1)
+        {
+            // UnityEngine.Debug.Log($"Setting target position {targetPosition}");
+        }
+        this.targetPosition = targetPosition * gearRatio[(int)jointId];
+    }
+
+    public void SetDiffPosition(float diffPosition)
+    {
+        this.diffPosition = diffPosition * gearRatio[(int)jointId];
+    }
+
+    public void SetTargetVelocity(float targetVelocity)
+    {
+        this.targetVelocity = targetVelocity * gearRatio[(int)jointId];
+        // if (jointId == JointId.Joint1)
+        // {
+        //     UnityEngine.Debug.Log($"Setting target velocity {this.targetVelocity}");
+        // }
+    }
+
+    public void SetDiffVelocity(float diffVelocity)
+    {
+        this.diffVelocity = diffVelocity * gearRatio[(int)jointId];
+    }
+}
--- unity_project/unity_sim/Assets/Simulation/Kalman/Arm/KalmanArm.cs
+++ unity_project/unity_sim/Assets/Simulation/Kalman/Arm/KalmanArm.cs
@@ -14,157 +14,16 @@ using UnityEngine.PlayerLoop;
 using MathNet.Numerics.LinearAlgebra;
 using UnityEngine.InputSystem.EnhancedTouch;
 
-public class KalmanArm : MonoBehaviour
+public class CanDriver
 {
-    class VirtualJoint
-    {
-        public Transform joint;
-        public int canId;
-
-        public enum ControlMode
-        {
-            SPEED,
-            POSITION,
-            POS_VEL,
-        }
-
-        public enum CanMessageIds
-        {
-            SET_VELOCITY = 0x025,
-            SET_POSITION = 0x026,
-            CONTROL_TYPE = 0x035,
-        }
-
-        public ControlMode mode;
-
-        public float position; // in deg
-
-        public float targetPosition;
-
-        public double velocity; // in RPM
-
-        public PID pid;
-
-        public float maxSpeed = 60.0f;
-
-        public float[] gearRatio = { 0.0125f, 0.00625f, 0.00625f, 0.01f, 0.0131034f, 0.0131034f };
-
-        public VirtualJoint(Transform joint, int canId)
-        {
-            this.joint = joint;
-            this.canId = canId;
-
-            mode = ControlMode.POS_VEL;
-            position = joint.rotation.eulerAngles[1];
-            targetPosition = position;
-            pid = new PID(10.0f, 0.01f, 0.01f);
-        }
-
-        public CanFdFrame GetStatusFrame()
-        {
-            CanFdFrame frame = new()
-            {
-                CanId = (uint)canId + 0x036,
-                Length = 6
-            };
-            frame.Data = new byte[frame.Length];
-            frame.Data[0] = 0x00;
-            frame.Data[1] = 0x00;
-            frame.Data[2] = 0x00;
-            frame.Data[3] = 0x00;
-            frame.Data[4] = 0x00;
-            frame.Data[5] = 0x00;
-            return frame;
-        }
-
-        public void ParseFrame(CanFdFrame frame)
-        {
-            // UnityEngine.Debug.Log($"Received frame on joint {joint} with id {frame.CanId}");
-            int msgId = (int)frame.CanId & (0x7F);
-            int jointId = ((int)frame.CanId >> 7) - 1;
-
-
-            switch (msgId)
-            {
-                case (int)CanMessageIds.SET_VELOCITY:
-                    velocity = BitConverter.ToInt16(frame.Data) * gearRatio[jointId] * 0.6;
-                    break;
-
-                case (int)CanMessageIds.SET_POSITION:
-                    // targetPosition = (float)(BitConverter.ToInt32(frame.Data) * 100.0);
-                    break;
-
-                case (int)CanMessageIds.CONTROL_TYPE:
-                    mode = (ControlMode)frame.Data[0];
-                    break;
-
-                default:
-                    break;
-            }
-
-        }
-
-
-        public void Update()
-        {
-            if (joint.name == "dof1")
-            {
-                UnityEngine.Debug.Log($"Updating joint {joint} with mode {mode}, position {position}, target {targetPosition}, velocity {velocity}");
-            }
-            if (this.mode == ControlMode.POS_VEL)
-            {
-                if (Math.Abs(velocity) > 0.001)
-                    targetPosition = (float)(position + velocity);
-            }
-            position += Mathf.Clamp(pid.Update(position, targetPosition, Time.fixedDeltaTime), -maxSpeed * Time.fixedDeltaTime, maxSpeed * Time.fixedDeltaTime);
-
-            // set only y rotation, with x and z not being changed
-            joint.localRotation = Quaternion.Euler(joint.localRotation.eulerAngles[0], position, joint.localRotation.eulerAngles[2]);
-
-
-        }
-    }
-
-    [SerializeField]
-    private List<Transform> armJoints = new List<Transform>();
     CanNetworkInterface vcan0 = CanNetworkInterface.GetAllInterfaces(true).First(iface => iface.Name.Equals("vcan0"));
 
-    [SerializeField]
-    private List<ArticulationBody> armArticulations = new List<ArticulationBody>();
-
-    private static readonly BlockingCollection<CanFdFrame> canFrames = new BlockingCollection<CanFdFrame>();
+    public static readonly BlockingCollection<CanFdFrame> canFrames = new BlockingCollection<CanFdFrame>();
     private static SafeFileDescriptorHandle socketHandle;
 
-    private static List<VirtualJoint> virtualJoints = new();
 
-    private void FixedUpdate()
+    public CanDriver(Action<CanFdFrame> frameHandler)
     {
-        foreach (VirtualJoint joint in virtualJoints)
-        {
-            joint.Update();
-        }
-    }
-
-    private static void HandleFrame(CanFdFrame frame)
-    {
-        int id = (int)frame.CanId >> 7;
-
-        if (id > 0 && id <= virtualJoints.Count)
-        {
-            // UnityEngine.Debug.Log($"Handling frame with joint id {id}");
-            virtualJoints[id - 1].ParseFrame(frame);
-            // UnityEngine.Debug.Log($"Hadled");
-        }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        for (int i = 0; i < armJoints.Count; i++)
-        {
-            virtualJoints.Add(new VirtualJoint(armJoints[i], i));
-        }
-
         socketHandle = LibcNativeMethods.Socket(SocketCanConstants.PF_CAN, SocketType.Raw, SocketCanProtocolType.CAN_RAW);
         int enableFdFrames = 1;
         LibcNativeMethods.SetSockOpt(socketHandle, SocketLevel.SOL_CAN_RAW, CanSocketOptions.CAN_RAW_FD_FRAMES, ref enableFdFrames, sizeof(int));
@@ -195,10 +54,9 @@ public class KalmanArm : MonoBehaviour
             return;
         }
 
-        Task.Run(() => PrintLoop());
+        Task.Run(() => HandleLoop(frameHandler));
         Task.Run(() => ReadFrames());
     }
-
 
     private static void ReadFrames()
     {
@@ -210,41 +68,211 @@ public class KalmanArm : MonoBehaviour
             int nReadBytes = LibcNativeMethods.Read(socketHandle, ref readFrame, frameSize);
             if (nReadBytes > 0)
             {
-                // UnityEngine.Debug.LogWarning($"Received frame on vcan0, passing its {nReadBytes} bytes");
                 canFrames.Add(readFrame);
+                // UnityEngine.Debug.Log($"Received frame with id {readFrame.CanId}, current count {canFrames.Count}");
             }
         }
     }
 
-
-
-
-    private static void PrintLoop()
+    private static void HandleLoop(Action<CanFdFrame> frameHandler)
     {
         while (true)
         {
             CanFdFrame frame = canFrames.Take();
-            // if (frame.CanId == 0xA5)
-            // {
-            //     if ((frame.CanId & (uint)CanIdFlags.CAN_RTR_FLAG) != 0)
-            //     {
-            //         UnityEngine.Debug.Log($":{SocketCanConstants.CAN_ERR_MASK & frame.CanId,8:X}   [{frame.Length:D2}]  RTR");
-            //     }
-            //     else
-            //     {
-            //         UnityEngine.Debug.Log($":{SocketCanConstants.CAN_ERR_MASK & frame.CanId,8:X}   [{frame.Length:D2}]  {BitConverter.ToString(frame.Data.Take(frame.Length).ToArray()).Replace("-", " ")}");
-            //     }
-            // }
-            HandleFrame(frame);
+            frameHandler(frame);
         }
     }
+}
+
+public enum CanMessageIds
+{
+    SET_VELOCITY = 0x025,
+    SET_POSITION = 0x026,
+    CONTROL_TYPE = 0x035,
+    GRIPPER = 0xE3,
+}
+public class KalmanArm : MonoBehaviour
+{
+    // class VirtualJoint
+    // {
+    //     public Transform joint;
+    //     public int canId;
+
+    //     public enum ControlMode
+    //     {
+    //         SPEED,
+    //         POSITION,
+    //         POS_VEL,
+    //     }
+
+    //     public enum CanMessageIds
+    //     {
+    //         SET_VELOCITY = 0x025,
+    //         SET_POSITION = 0x026,
+    //         CONTROL_TYPE = 0x035,
+    //     }
+
+    //     public ControlMode mode;
+
+    //     public float position; // in deg
+
+    //     public float targetPosition;
+
+    //     public double velocity; // in RPM
+
+    //     public PID pid;
+
+    //     public float maxSpeed = 60.0f;
+
+    //     public float[] gearRatio = { 0.0125f, 0.00625f, 0.00625f, 0.01f, 0.0131034f, 0.0131034f };
+
+    //     public VirtualJoint(Transform joint, int canId)
+    //     {
+    //         this.joint = joint;
+    //         this.canId = canId;
+
+    //         mode = ControlMode.POS_VEL;
+    //         position = joint.rotation.eulerAngles[1];
+    //         targetPosition = position;
+    //         pid = new PID(10.0f, 0.01f, 0.01f);
+    //     }
+
+    //     public CanFdFrame GetStatusFrame()
+    //     {
+    //         CanFdFrame frame = new()
+    //         {
+    //             CanId = (uint)canId + 0x036,
+    //             Length = 6
+    //         };
+    //         frame.Data = new byte[frame.Length];
+    //         frame.Data[0] = 0x00;
+    //         frame.Data[1] = 0x00;
+    //         frame.Data[2] = 0x00;
+    //         frame.Data[3] = 0x00;
+    //         frame.Data[4] = 0x00;
+    //         frame.Data[5] = 0x00;
+    //         return frame;
+    //     }
+
+    //     public void ParseFrame(CanFdFrame frame)
+    //     {
+    //         // UnityEngine.Debug.Log($"Received frame on joint {joint} with id {frame.CanId}");
+    //         int msgId = (int)frame.CanId & (0x7F);
+    //         int jointId = ((int)frame.CanId >> 7) - 1;
+
+
+    //         switch (msgId)
+    //         {
+    //             case (int)CanMessageIds.SET_VELOCITY:
+    //                 velocity = BitConverter.ToInt16(frame.Data) * gearRatio[jointId] * 0.6;
+    //                 break;
+
+    //             case (int)CanMessageIds.SET_POSITION:
+    //                 // targetPosition = (float)(BitConverter.ToInt32(frame.Data) * 100.0);
+    //                 break;
+
+    //             case (int)CanMessageIds.CONTROL_TYPE:
+    //                 mode = (ControlMode)frame.Data[0];
+    //                 break;
+
+    //             default:
+    //                 break;
+    //         }
+
+    //     }
+
+
+    //     public void Update()
+    //     {
+    //         if (joint.name == "dof1")
+    //         {
+    //             UnityEngine.Debug.Log($"Updating joint {joint} with mode {mode}, position {position}, target {targetPosition}, velocity {velocity}");
+    //         }
+    //         if (this.mode == ControlMode.POS_VEL)
+    //         {
+    //             if (Math.Abs(velocity) > 0.001)
+    //                 targetPosition = (float)(position + velocity);
+    //         }
+    //         position += Mathf.Clamp(pid.Update(position, targetPosition, Time.fixedDeltaTime), -maxSpeed * Time.fixedDeltaTime, maxSpeed * Time.fixedDeltaTime);
+
+    //         // set only y rotation, with x and z not being changed
+    //         joint.localRotation = Quaternion.Euler(joint.localRotation.eulerAngles[0], position, joint.localRotation.eulerAngles[2]);
+
+
+    //     }
+    // }
+
+    [SerializeField]
+    public List<GameObject> armObjects = new List<GameObject>();
+
+    private CanDriver driver;
+
+    private static Dictionary<JointId, JointController> armControllers = new Dictionary<JointId, JointController>();
+
+    private static void HandleFrame(CanFdFrame frame)
+    {
+        int id = (int)frame.CanId >> 7;
+        int msgId = (int)frame.CanId & 0x7F;
+
+        if (frame.CanId == (uint)CanMessageIds.GRIPPER)
+        {
+            UnityEngine.Debug.Log($"Setting gripper to {BitConverter.ToUInt16(frame.Data)}");
+            armControllers[JointId.Gripper].SetTargetPosition((2600 - BitConverter.ToUInt16(frame.Data)) * 100.0f / 1230.0f);
+        }
+        else if (frame.CanId == (uint)CanMessageIds.CONTROL_TYPE)
+        {
+            // UnityEngine.Debug.Log($"Setting control type to {BitConverter.ToInt32(frame.Data)}");
+            foreach (JointController controller in armControllers.Values)
+            {
+                controller.SetControlMode((ControlMode)frame.Data[0]);
+            }
+        }
+        else if (id > 0 && id <= armControllers.Count)
+        {
+            switch (msgId)
+            {
+                case (int)CanMessageIds.SET_VELOCITY:
+                    if (JointController.jointTypes[(JointId)(id - 1)] == JointType.Differential)
+                    {
+                        armControllers[JointController.differentialJoints[(JointId)id - 1]].SetDiffVelocity(BitConverter.ToInt16(frame.Data) * 0.6f);
+                    }
+                    armControllers[(JointId)id - 1].SetTargetVelocity(BitConverter.ToInt16(frame.Data) * 0.6f);
+                    break;
+
+                case (int)CanMessageIds.SET_POSITION:
+                    if (JointController.jointTypes[(JointId)(id - 1)] == JointType.Differential)
+                    {
+                        armControllers[JointController.differentialJoints[(JointId)id - 1]].SetDiffPosition(BitConverter.ToInt32(frame.Data) / 100.0f);
+                    }
+                    armControllers[(JointId)id - 1].SetTargetPosition(BitConverter.ToInt32(frame.Data) / 100.0f);
+                    break;
+
+                default:
+                    UnityEngine.Debug.Log($"Unknown message id {msgId}");
+                    break;
+            }
+        }
+        else
+        {
+            UnityEngine.Debug.LogWarning($"Unknown joint id {id}");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject armObject in armObjects)
+        {
+            JointController controller = armObject.GetComponent<JointController>();
+            armControllers.Add(controller.jointId, controller);
+        }
+        driver = new CanDriver(HandleFrame);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // foreach (Transform joint in armJoints)
-        // {
-        //     joint.Rotate(Vector3.up, 1);
-        // }
+        // UnityEngine.Debug.Log($"{CanDriver.canFrames.Count} frames in queue");
 
     }
 }
